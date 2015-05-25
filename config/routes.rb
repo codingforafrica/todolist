c0cd0c51fc88de67d6dba6524c7b87df601a4768
unknown <codingforafrica@gmail.com>
2015-05-25 05:14:00 +0000

    Perfected the Authentication views:

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'support' => 'pages#support'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
